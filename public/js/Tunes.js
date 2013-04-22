14f351652d86f54918e9a8f1fef9ede781aac12f
lkbgift <lkbgift@gmail.com>
2013-04-22 10:37:53 -0400

    setting up album model and view

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,5 +1,18 @@
 (function($) {
 
-  Album = Backbone.Model.extend({});
+  window.Album = Backbone.Model.extend({});
+
+  window.AlbumView = Backbone.View.extend({
+
+    intialize: function(){
+      this.template = _.template($('#album-template'.html()));
+    },
+
+    render: function(){
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+    }
+
+  });
 
 })(jQuery);
